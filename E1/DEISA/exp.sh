b862858404acdd969ba4f85e1dde3c9377acea07
maul0803 <quintyn.c@hotmail.com>
2024-12-31 14:59:41 +0100

    Initial commit

---
+++ E1/DEISA/exp.sh
@@ -0,0 +1,7 @@
+# ./Launcher.sh P1 P2 P/NODE D1 D2 IT AN WORKER/N THR/W PROGRAM
+
+########################### DERIVATIVE
+./Launcher.sh 2 2 2 8192 4096 12 2 30 1 derivative;
+
+########################### REDUCTION
+./Launcher.sh 2 2 2 8192 4096 25 2 30 1 reduction;
